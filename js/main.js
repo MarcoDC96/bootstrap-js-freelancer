fa11d88893638f4ef3aa6f0fede0a4ad15f635c7
MarcoDC96 <marco.decicco@live.it>
2023-11-27 10:59:00 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-function calculateOffer(event) {
-    event.preventDeFault();
+let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
-let works = document.getElementById("typeWorks").value
-let hour = 
+function submitForm(event) {
+    event.preventDefault();
+    let userHours = document.getElementById("hours").value;
+    let typeWork = document.getElementById("type-of-work").value;
+    let userDiscount = document.getElementById("discount-code").value;
+
+    let price = 0;
+    if (typeWork == "backend") {
+        price = userHours * 20.5;
+        typeWork = "Backend Development"
+    } else if (typeWork == "frontend") {
+        price = userHours * 15.3;
+        typeWork = "Frontend Development"
+    } else if (typeWork == "analysis") {
+        price = userHours * 33.6;
+        typeWork = "Project Analysis"
+    }
+
 
 }
