071ff36e0c2aa02d6d3567d1dbb1e3fb35bf1582
MarcoDC96 <marco.decicco@live.it>
2023-11-27 11:50:38 +0100

    Finish without bonus

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
-function submitForm(event) {
+function calculateOffer(event) {
     event.preventDefault();
     let userHours = document.getElementById("hours").value;
     let typeWork = document.getElementById("type-of-work").value;
     let userDiscount = document.getElementById("discount-code").value;
 
-    let price = 0;
+    let prezzo = 0;
     if (typeWork == "backend") {
-        price = userHours * 20.5;
+        prezzo = userHours * 20.5;
         typeWork = "Backend Development"
     } else if (typeWork == "frontend") {
-        price = userHours * 15.3;
+        prezzo = userHours * 15.3;
         typeWork = "Frontend Development"
-    } else if (typeWork == "analysis") {
-        price = userHours * 33.6;
+    } else if (typeWork == "projectAnalysis") {
+        prezzo = userHours * 33.6;
         typeWork = "Project Analysis"
     }
 
+    if (discountCodes.includes(userDiscount)) {
+        prezzo = prezzo / 100 * 75;
+    }
 
+    else if (userDiscount.length >= 1 && !discountCodes.includes(userDiscount)) {
+        document.getElementById("discountError").classList.remove("d-none");
+        return false;
+    }
+
+    document.getElementById("offerForm").value;
+    document.getElementById("finalContainer").value;
+
+    document.getElementById("priceId").innerHTML = "The Price is: " + prezzo.toFixed(2) + " €";
+    document.getElementById("hoursId").innerHTML = "Hours requested: " + userHours;
+    document.getElementById("workId").innerHTML = "Type of work: " + typeWork;
 }
